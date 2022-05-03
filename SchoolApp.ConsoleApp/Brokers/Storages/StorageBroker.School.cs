96cd7a7e22b6a40bb1b818a78435d898baa68797
Mahalakshmi Murari <murarimaha@outlook.com>
2022-05-03 15:19:34 -0700

    BROKER: Insert School

---
+++ SchoolApp.ConsoleApp/Brokers/Storages/StorageBroker.School.cs
@@ -0,0 +1,20 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System.Collections.Generic;
+using SchoolApp.ConsoleApp.Models.Schools;
+
+namespace SchoolApp.ConsoleApp.Brokers.Storages
+{
+    internal partial class StorageBroker : IStorageBroker
+    {
+        List<School> Schools = new List<School>();
+        public School insertSchool(School school)
+        {
+            Schools.Add(school);
+
+            return school;
+        }
+    }
+}
---
+++ SchoolApp.ConsoleApp/Models/Schools/School.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+
+namespace SchoolApp.ConsoleApp.Models.Schools
+{
+    internal class School
+    {
+        public string SchoolName { get; set; }
+        public string SchoolDescription { get; set; }
+        public Guid SchoolId { get; set; }
+        public string SchoolLocation { get; set; }
+        public string CountyName { get; set; }
+    }
+}
