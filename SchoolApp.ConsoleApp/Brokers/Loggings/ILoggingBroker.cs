af7ddbce6a66f6554d066abd858c41fac12d0636
Mahalakshmi Murari <murarimaha@outlook.com>
2022-05-12 18:40:32 -0700

    Added Logging Broker

---
+++ SchoolApp.ConsoleApp/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,13 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+
+namespace SchoolApp.ConsoleApp.Brokers.Loggings
+{
+    internal interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+    }
+}
