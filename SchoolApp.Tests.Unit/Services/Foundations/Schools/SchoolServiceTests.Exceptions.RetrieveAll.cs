8c89755455c2df5ed9dc5fff3f0e9ecbf64d4b2d
Mahalakshmi Murari <murarimaha@outlook.com>
2022-06-01 16:35:47 -0700

    ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogIt -> FAIL

---
+++ SchoolApp.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,49 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using Moq;
+using SchoolApp.ConsoleApp.Models.Schools.Exceptions;
+using Xunit;
+
+namespace SchoolApp.Tests.Unit.Services.Foundations.Schools
+{
+    public partial class SchoolServiceTests
+    {
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogIt()
+        {
+            //given
+            var serviceException = new Exception();
+
+            var failedSchoolServiceException =
+                new FailedSchoolServiceException(serviceException);
+
+            var expectedSchoolServiceException =
+                new SchoolServiceException(failedSchoolServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllSchools())
+                    .Throws(serviceException);
+
+            //when
+            Action retrieveAllAction = () => this.schoolService.RetrieveAllSchools();
+
+            //then
+            Assert.Throws<SchoolServiceException>(retrieveAllAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllSchools(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSchoolServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
