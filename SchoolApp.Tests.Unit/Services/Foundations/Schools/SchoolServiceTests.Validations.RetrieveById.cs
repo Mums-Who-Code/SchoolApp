54f86103dab923475f6cacce32bccb7b6494fe0e
Mahalakshmi Murari <murarimaha@outlook.com>
2022-06-15 13:27:25 -0700

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogIt -> FAIL

---
+++ SchoolApp.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,49 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using Moq;
+using SchoolApp.ConsoleApp.Models.Schools;
+using SchoolApp.ConsoleApp.Models.Schools.Exceptions;
+using Xunit;
+
+namespace SchoolApp.Tests.Unit.Services.Foundations.Schools
+{
+    public partial class SchoolServiceTests
+    {
+        [Fact]
+        public void ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogIt()
+        {
+            //given
+            var invalidSchool = new School();
+            var invalidSchoolException = new InvalidSchoolException();
+
+            invalidSchoolException.AddData(
+                key: nameof(School.SchoolId),
+                values: "ID is required");
+
+            var expectedSchoolValidationException =
+                new SchoolValidationException(invalidSchoolException);
+
+            //when
+            Action retrieveSchoolByIdAction = () => 
+                this.schoolService.RetrieveSchoolById(invalidSchool.SchoolId);
+
+            //then
+            Assert.Throws<SchoolValidationException>(retrieveSchoolByIdAction);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSchoolValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSchoolById(It.IsAny<int>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
