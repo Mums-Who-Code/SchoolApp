06748157cb3b468e47aab96575c459d2b66db3ba
Mahalakshmi Murari <murarimaha@outlook.com>
2022-06-17 16:54:51 -0700

    ShouldThrowDependencyValidationExceptionOnRetrieveByIdIfValidationErrorOccursAndLogIt -> FAIL

---
+++ SchoolApp.ConsoleApp/Models/Schools/Exceptions/SchoolDependencyValidationException.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Xeptions;
+
+namespace SchoolApp.ConsoleApp.Models.Schools.Exceptions
+{
+    public class SchoolDependencyValidationException : Xeption
+    {
+        public SchoolDependencyValidationException(Xeption innerException)
+            : base(message: "School dependency validation error occured,fix the errors and try again.",
+                  innerException)
+        { }
+    }
+}
---
+++ SchoolApp.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,52 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using Moq;
+using SchoolApp.ConsoleApp.Models.Schools.Exceptions;
+using Xunit;
+
+namespace SchoolApp.Tests.Unit.Services.Foundations.Schools
+{
+    public partial class SchoolServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyValidationExceptionOnRetrieveByIdIfValidationErrorOccursAndLogIt()
+        {
+            //given
+            int someSchoolId = GetRandomNumber();
+            var argumentNullException = new ArgumentNullException();
+
+            var nullArgumentSchoolException =
+                new NullArgumentSchoolException(argumentNullException);
+
+            var expectedSchoolDependencyValidationException =
+                new SchoolDependencyValidationException(
+                    nullArgumentSchoolException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSchoolById(It.IsAny<int>()))
+                    .Throws(argumentNullException);
+
+            //when
+            Action retrieveSchoolByIdAction = () =>
+                this.schoolService.RetrieveSchoolById(someSchoolId);
+
+            //then
+            Assert.Throws<SchoolDependencyValidationException>(retrieveSchoolByIdAction);
+
+            this.storageBrokerMock.Verify(broker =>
+              broker.SelectSchoolById(It.IsAny<int>()),
+                  Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSchoolDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
